213bdbb06e5126ee90bc90d7a72b0d39740d964a
nicolasshn98 <74858078+nicolasshn98@users.noreply.github.com>
2021-04-06 11:26:07 +0200

    Add files via upload

---
+++ movee/src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.carousel__photo').slick({
+		speed: 1200,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>'
+	});
+});
